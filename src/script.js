b20613e3978c250d3393d9ea177521a89554c942
Almeeida <rodrigofigueire19@gmail.com>
2019-09-28 18:23:58 +0100

    it has been done

--- src/script.js
+++ src/script.js
@@ -1,20 +1,26 @@
-function aasdasdsa() {
-  console.log(document.getElementsByClassName('textview').value);
-} 
+function add(x) {
+  const exp = ['/', '+', '-', '*', '.'];
+  const current = document.getElementById('history').innerText;
 
-let textview = '';
-let result = null;
-const exp = ['/', '+', '-', '*'];
-function solve(a) {
-  document.getElementsByClassName('textview').value = '555'
+  if (exp.includes(x) && exp.find(e => current.endsWith(e))) return;
+  
+  document.getElementById('history').innerText += x;
+  const result = eval(document.getElementById('history').innerText);
+  if (result) document.getElementById('output').innerText = result;
 }
 
-function insert () {
-  if (exp.includes(a)) {
-      // nao add a mesma expressão duas vezes
-    if (textview.endsWith(a)) return
-    textview += a + ' ';
-  } else {
-    textview += a;
-  }
+function clean() {
+  document.getElementById('history').innerText = '';
+  document.getElementById('output').innerText = '';
+}
+
+function igual() {
+  document.getElementById('history').innerText = document.getElementById('output').innerText;
+  document.getElementById('output').innerText = '';
+}
+
+function deleteLast() {
+  document.getElementById('history').innerText = document.getElementById('history').innerText.slice(0, -1);
+  const result = eval(document.getElementById('history').innerText);
+  if (result) document.getElementById('output').innerText = result;
 }
